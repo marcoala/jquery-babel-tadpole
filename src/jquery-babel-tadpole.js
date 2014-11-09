799abc6a59716fff47f9caafb53b5b2fd4243b0e
marcoala <marcoala@users.noreply.github.com>
2014-11-09 20:18:41 +0100

    Refactor code

--- src/jquery-babel-tadpole.js
+++ src/jquery-babel-tadpole.js
@@ -1,36 +1,70 @@
 (function($) {
+
 	$.fn.babelTadpole = function(options) {
 
-		var settings = $.extend( {
-			'defaultLanguage' : '',
-			'aviableLanguages': [],
-			'autogenerateCSS': true,
-			'languageSelector': '.language-selection li a',
-			'languageSelectorAttribute' : 'data-lang',
-			'service' : 'https://ajaxhttpheaders1.appspot.com',
-
-			//callback
-			'onLanguageAutomaticSelection' : function(){},
-			'onLanguageChange' : function(){}
-		}, options);
-
-		var flagLanguageAutoSelected = false;
+		var settings = $.extend({},$.fn.babelTadpole.default , options);
 
 		if(settings.autogenerateCSS){
-			var style = '';
-			var styleTemplate = 'span.LANG{display: none;transition: display 0s !important;}' +
-								'body.LANG span.LANG{display: inline;}';
-
-			for (i = 0; i < settings.aviableLanguages.length; i++) {
-			    style = style + styleTemplate.replace(/LANG/g, settings.aviableLanguages[i]) + "\n";
-			}
-
-			style = "<style>" + style + "</style>";
+			var style = generateCSS(settings);
 			$(style).appendTo("head");
 		}
 
-
 		//detect user language by http header
+		var match = matchAcceptLanguageWithAviableLanguages(settings);
+
+		if(match !== false){
+			setLanguage(settings, match, true);
+		}else if(isAvaiableLanguage(settings, getBrowserLanguage())){
+		    setLanguage(settings, getBrowserLanguage(), true);
+		}else{
+			setLanguage(settings, settings.defaultLanguage, true);
+		}
+
+		//change language by menu
+		$(settings.languageSelector).click(function(e){
+			e.preventDefault();
+			language = $(this).attr(settings.languageSelectorAttribute);
+			if(isAvaiableLanguage(settings, language)){
+				setLanguage(settings, language);
+			}
+		});
+
+		return this; 
+	};
+
+	var generateCSS = function(settings){
+		var style = '';
+		var styleTemplate = 'span.LANG{display: none;transition: display 0s !important;}' +
+							'body.LANG span.LANG{display: inline;}';
+
+		for (i = 0; i < settings.aviableLanguages.length; i++) {
+		    style = style + styleTemplate.replace(/LANG/g, settings.aviableLanguages[i]) + "\n";
+		}
+
+		style = "<style>" + style + "</style>";
+		return style;
+	}
+
+	var isAvaiableLanguage = function(settings, language){
+		var isAvaiable = false;
+		if($.inArray(language , settings.aviableLanguages ) > -1){
+		    isAvaiable = true;
+		}
+
+		return isAvaiable;
+	}
+
+	var setLanguage = function(settings, language, isAutoSelect = false){
+		$("body").removeClass(settings.aviableLanguages.join(" ")).addClass(language);
+		if(isAutoSelect){
+			settings.onLanguageAutomaticSelection.call(this);
+		}
+		settings.onLanguageChange.call(this);
+	}
+
+	var matchAcceptLanguageWithAviableLanguages = function(settings){
+		var flagLanguageAutoSelected = false;
+
 		$.ajax({
 		  url: settings.service, 
 		  dataType: 'jsonp',
@@ -39,35 +73,36 @@
 		      languages = [];
 		      for (index in headerLanguages ){
 		          language = headerLanguages[index].substring(0,2).toLowerCase();
-		          if($.inArray(language , settings.aviableLanguages ) > -1){
-		              $("body").removeClass(settings.aviableLanguages.join(" ")).addClass(language);
+		          if(isAvaiableLanguage(settings, language)){
 		              flagLanguageAutoSelected = true;
-		              settings.onLanguageAutomaticSelection.call( this );
 		              break;
 		          }
 		      }
 		  }
 		});
 
-		//if http header method fails use browser language
 		if(!flagLanguageAutoSelected){
-			browserLanguages = window.navigator.userLanguage || window.navigator.language;
-			language = browserLanguages.substring(0,2).toLowerCase();
-
-			if($.inArray(language , settings.aviableLanguages ) > -1){
-			    $("body").removeClass(settings.aviableLanguages.join(" ")).addClass(language);
-			     settings.onLanguageAutomaticSelection.call( this );
-			}
+			return false;
 		}
+		
+		return language;
+	}
 
-		//change language by menu
-		$(settings.languageSelector).click(function(e){
-			e.preventDefault();
-			language = $(this).attr(settings.languageSelectorAttribute);
-			$("body").removeClass(settings.aviableLanguages.join(" ")).addClass(language);
-			 settings.onLanguageChange.call( this );
-		});
+	var getBrowserLanguage = function(){
+		browserLanguages = window.navigator.userLanguage || window.navigator.language;
+		return browserLanguages.substring(0,2).toLowerCase();
+	}
 
-		return this; 
+	$.fn.babelTadpole.default = {
+		'defaultLanguage' : '',
+		'aviableLanguages': [],
+		'autogenerateCSS': true,
+		'languageSelector': '.language-selection li a',
+		'languageSelectorAttribute' : 'data-lang',
+		'service' : 'https://ajaxhttpheaders1.appspot.com',
+
+		//callback
+		'onLanguageAutomaticSelection' : function(){},
+		'onLanguageChange' : function(){}
 	};
 })( jQuery );
